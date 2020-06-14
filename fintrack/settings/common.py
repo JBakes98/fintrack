e727e393f86b596171559bbe4f39a744086d3609
josh <joshbaker286.jb@gmail.com>
2020-06-14 23:55:57 +0100

    correct common template setting to use the correct path

--- fintrack/settings/common.py
+++ fintrack/settings/common.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'fintrack', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'fintrack_be', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
