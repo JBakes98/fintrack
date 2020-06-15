9ca91d7954dae262c991581278c0a9dec96c7ee6
josh <joshbaker286.jb@gmail.com>
2020-06-15 16:14:46 +0100

    trying to configure static

--- fintrack/settings/common.py
+++ fintrack/settings/common.py
@@ -111,9 +111,6 @@ USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
