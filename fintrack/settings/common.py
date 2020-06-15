7b16aae4d72f760adb7e73982ed4e2312e524c64
josh <joshbaker286.jb@gmail.com>
2020-06-15 14:33:32 +0100

    changed static files path

--- fintrack/settings/common.py
+++ fintrack/settings/common.py
@@ -111,8 +111,6 @@ USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
+
